3ced23696a6e4261d2864a73c45e62ea0615d95e
Adam <296786475@qq.com>
2022-01-12 15:43:34 +0800

    Initial Commit

---
+++ Adam_20220112_Talented/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adam_20220112_Talented
+//
+//  Created by Adam on 2022/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
