a8aba13544e6871b5f2391ae87ae785739e6f428
Adam <296786475@qq.com>
2022-01-14 17:54:26 +0800

    1.change to iOS 14. 2.Delete Main.storyboard.

---
+++ Adam_20220112_Talented/CalculatorMainView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorMainView.swift
+//  Adam_20220112_Talented
+//
+//  Created by Adam on 2022/1/14.
+//
+
+import SwiftUI
+
+struct CalculatorMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorMainView()
+    }
+}
--- Adam_20220112_Talented/SceneDelegate.swift
+++ Adam_20220112_Talented/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SwiftUI
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +18,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        context?.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        if let windowScene = scene as? UIWindowScene {
+            // 用UIHostingController把SwiftUI转化为UIKit的UIViewController
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: CalculatorMainView().environment(\.managedObjectContext, context!))
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+//        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
